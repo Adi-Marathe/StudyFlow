61bce41e43c207cbb444d8460c46a21be0bb62e2
Adi-Marathe <adimarathe234@gmail.com>
2025-08-14 22:54:07 +0530

    pomodoro and falshcard main page srcollable now

--- client/src/Pages/Flashcards/Flashcards.js
+++ client/src/Pages/Flashcards/Flashcards.js
@@ -1,29 +1,34 @@
-import react from 'react';
+import React from 'react';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import './Flashcards.css';
 import Navbar from '../../Components/Navbar/Navbar';
 import WelcomeBanner from '../../Components/WelcomeBanner/WelcomeBanner';
 
 function Flashcards() {
-    return(
-        <>
-          <div className='flashcards-container'>
-           <Sidebar/>
-           <main className="flashcards-main">
-              <Navbar/>
+    return (
+    <div className='flashcards-container'>
+      <Sidebar />
+      
+      <div className="flashcards-content-wrapper">
+        <Navbar />
+        <main className="flashcards-main">
+          <div style={{ height: "2000px" }}>
+            <div className='sf-welcome-header'>
               <WelcomeBanner
-              subtitle="Welcome To"
-              title="Your Flashcard Learning Hub"
-              description="Master concepts faster with quick reviews and smart recall. Let’s turn revision into a fun daily habit!"
-              buttonText="+ Add Flashcards"
-              onButtonClick={() => console.log("Open Task Modal")}
-              animation = "https://lottie.host/702419b2-3618-462f-8426-c6f1e65cbb22/wNiyt44PGI.lottie"
+                  subtitle="Welcome To"
+                  title="Your Flashcards Learning Hub"
+                  description="Master concepts faster with quick reviews and smart recall. Let’s turn revision into a fun daily habit!"
+                  buttonText="+ Add Flashcards"
+                  onButtonClick={() => console.log("Open Task Modal")}
+                //   animation="https://lottie.host/702419b2-3618-462f-8426-c6f1e65cbb22/wNiyt44PGI.lottie"
               />
-           </main>             
-        </div>
-          
-        </>
-    );
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default Flashcards;
+
--- client/src/Pages/Pomodoro/Pomodoro.js
+++ client/src/Pages/Pomodoro/Pomodoro.js
@@ -5,24 +5,30 @@ import Navbar from '../../Components/Navbar/Navbar';
 import WelcomeBanner from '../../Components/WelcomeBanner/WelcomeBanner';
 
 function Pomodoro() {
-    return(
-        <>
-         <div className='pomodoro-container'>
-           <Sidebar/>
-           <main className="pomodoro-main">
-              <Navbar/>
-              <WelcomeBanner
-              subtitle="Welcome To"
-              title="Your Pomodoro Focus Zone "
-              description="Boost your productivity with focused sprints and refreshing breaks. Let’s conquer distractions—one Pomodoro at a time!"
-              buttonText="+ Start Timer"
-              onButtonClick={() => console.log("Open Add Modal")}
-              animation = "https://lottie.host/b77d9734-2bb2-4498-8b33-ab3e6031c489/pNu8gOmDjV.lottie"
-              />
-           </main>  
-         </div>
-        </>
+    return (
+      <div className='pomodoro-container'>
+        <Sidebar />
+        
+        <div className="pomodoro-content-wrapper">
+          <Navbar />
+          <main className="pomodoro-main">
+            <div style={{ height: "2000px" }}>
+              <div className='sf-welcome-header'>
+                <WelcomeBanner
+                subtitle="Welcome To"
+                title="Your Pomodoro Focus Zone "
+                description="Boost your productivity with focused sprints and refreshing breaks. Let’s conquer distractions—one Pomodoro at a time!"
+                buttonText="+ Start Timer"
+                onButtonClick={() => console.log("Open Add Modal")}
+                animation = "https://lottie.host/b77d9734-2bb2-4498-8b33-ab3e6031c489/pNu8gOmDjV.lottie"
+                />
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
     );
 }
 
 export default Pomodoro;
+
